b9ec7569b7e2fc063c31a821bdf4e9e0107b4616
Mauricio <82846155+MauriManX@users.noreply.github.com>
2021-11-08 08:51:21 -0700

    Add files via upload
    Version del 8 de noviembre

---
+++ EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/EVA2_7_LISTA_SIMPLE.java
@@ -0,0 +1,16 @@
+package eva2_7_lista_simple;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class EVA2_7_LISTA_SIMPLE {
+    
+//Se necesitan dos clases, una lista y una nodo
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
