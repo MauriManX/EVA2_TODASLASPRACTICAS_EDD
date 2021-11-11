3536259adb0d26c53435b802b07897e9d14d57b8
Mauricio <82846155+MauriManX@users.noreply.github.com>
2021-11-11 09:58:08 -0700

    Add files via upload

--- EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/EVA2_7_LISTA_SIMPLE.java
+++ EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/EVA2_7_LISTA_SIMPLE.java
@@ -10,6 +10,24 @@ public class EVA2_7_LISTA_SIMPLE {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        //Crear una lista
+        Lista miLista = new Lista();//Se crea lista vacía}
+        /*miLista.printList();
+        Nodo nodoNuevo = new Nodo(100);
+        miLista.add(nodoNuevo);
+        miLista.add(new Nodo(200));
+        miLista.add(new Nodo(300));
+        miLista.add(new Nodo(400));
+        miLista.add(new Nodo(500));
+        miLista.printList();*/
+        int [] miArreglo = new int [1000000];
+        for(int i = 0; i<miArreglo.length;i++){
+            miArreglo[i] = (int) (Math.random()*1000);
+        }
+        
+        for(int i = 0; i<1000000;i++){
+            miLista.add(new Nodo ((int) (Math.random()*1000)));
+        }
         
     }
     
--- EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/Lista.java
+++ EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/Lista.java
@@ -11,9 +11,11 @@ package eva2_7_lista_simple;
  */
 public class Lista { //Lista simplemente enlazada
     private Nodo inicio;//Es el punto de incio y acceso a la lista
+    private Nodo fin;
     
     public Lista(){
         inicio = null; //Lista vacia
+        fin = null;
     }
     
     //Agregar nodos
@@ -21,14 +23,28 @@ public class Lista { //Lista simplemente enlazada
         
         if(inicio==null){
             inicio=nuevo; //Se conecta el primer nodo a la lista
+            fin = nuevo;
         }
         else{//Cuando la lista con nodos...
             //Se debe ir hasta el final de la lista
             //Usando temp se recorrerá nodo por nodo hasta llegar al final
             Nodo temp = inicio;//Desde aqui se empieza
-            
+            //while
+            while(temp.getSiguiente() != null){//Mientras temp.siguiente != seguiremos avanzando nodo tras nodo
+                temp = temp.getSiguiente();//Se mueve temp al nodo siguiente
+      
+            }
+            temp.setSiguiente(nuevo);//Conectamos el nodo nuevo al final de la lista
             
         }
     }
     //Imprimir Lista
+    public void printList(){
+        Nodo temp = inicio;//Desde aqui se empieza
+            //while
+            while(temp != null){//Mientras temp != seguiremos avanzando
+                System.out.print(temp.getDatito() + " - ");
+                temp = temp.getSiguiente();//Temo se mueve al siguiente nodo
+            }
+    }
 }
