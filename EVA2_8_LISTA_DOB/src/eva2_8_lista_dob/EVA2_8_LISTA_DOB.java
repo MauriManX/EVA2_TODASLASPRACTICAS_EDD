ddae1a0e74d3482bf1f87d8bfed0ea7b62d61c88
Mauricio <82846155+MauriManX@users.noreply.github.com>
2021-11-29 09:01:16 -0700

    Add files via upload

--- EVA2_8_LISTA_DOB/src/eva2_8_lista_dob/EVA2_8_LISTA_DOB.java
+++ EVA2_8_LISTA_DOB/src/eva2_8_lista_dob/EVA2_8_LISTA_DOB.java
@@ -16,6 +16,17 @@ public class EVA2_8_LISTA_DOB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ListaDoble listaDoble = new ListaDoble();
+        listaDoble.add(100);
+        listaDoble.add(200);
+        listaDoble.add(300);
+        listaDoble.add(400);
+        listaDoble.add(500);
+        System.out.println("Cantidad de Nodos: "+listaDoble.length());
+        System.out.println("");
+        listaDoble.insertAt(0, 0);
+        System.out.println("");
+        System.out.println("Cantidad de Nodos: "+listaDoble.length());
     }
     
 }
--- EVA2_8_LISTA_DOB/src/eva2_8_lista_dob/ListaDoble.java
+++ EVA2_8_LISTA_DOB/src/eva2_8_lista_dob/ListaDoble.java
@@ -33,5 +33,92 @@ public class ListaDoble { //Lista simplemente enlazada
         }
         tama++;
     }
+    //Imprimir Lista
+    public void printList(){//O(N)
+        Nodo temp = inicio;//Desde aqui se empieza
+            //while
+            while(temp != null){//Mientras temp != seguiremos avanzando
+                System.out.print(temp.getDatito() + " - ");
+                temp = temp.getSiguiente();//Temo se mueve al siguiente nodo
+            }
+    }
+    //Tarea imprimir lista inversa
+    //Borrar la lista
+    public void clear(){//O(1)
+        inicio = null;
+        fin = null;
+        tama=0;
+    }
     
+    //Contar los nodos
+    public int length(){
+        /*int iCont = 0;
+        Nodo temp = inicio;//Desde aqui se empieza
+            //while
+            while(temp != null){//Mientras temp != seguiremos avanzando
+                iCont++;
+                temp = temp.getSiguiente();//Temo se mueve al siguiente nodo
+            }
+            return iCont;*/
+            return tama;
+    }
+    
+    //La lista está vacía: True, con Nodos: False
+    public boolean isEmpty(){
+        //Verificacion
+        if(inicio == null)
+            return true;
+        else
+            return false;
+    }
+    
+    //Recuperar un valor de la lista
+    public int get(int pos) throws Exception{
+        //verificacion
+        if(isEmpty()){//Lista vacía
+            throw new Exception("No hay valores almacenados en la lista");
+        }else{//Posicion que no exista: Posicion negativa o que nos pasemos
+            //pos tiene que estar entre 0 y n-1 --> n es el tamaño (length)
+            if((pos < 0) || (pos >= length() ))//Si pod es menor a cero o por es mayor o igual a n
+              throw new Exception("Esa posicion no existe dentro de la lista");  
+        }
+     
+        Nodo temp = inicio;
+        for(int i = 0; i < pos ;i++){
+            temp = temp.getSiguiente();//Temo se mueve al siguiente nodo
+        }
+        return temp.getDatito();
+    }
+    
+    private void insertAtBegining(Nodo nuevo){//O(1)
+        nuevo.setSiguiente(inicio);
+        inicio.setPrevio(nuevo);
+        
+        
+    }
+    
+    public void insertAt (int pos,int valor){// O(N)
+        Nodo nuevo = new Nodo(valor);
+        //Validar
+        
+        //Situaciones
+        //Insertar al inicio
+        if(pos == 0){
+            insertAtBegining(nuevo);
+        }else{//Cualquier otro caso
+            Nodo temp = inicio;//Desde aqui se empieza
+            for (int i = 0; i<pos; i++){
+                temp = temp.getSiguiente();//Mover temp al siguiente nodo
+            }
+            //Falta reconectar
+            //Primero conectamos el nievo nodo
+            nuevo.setSiguiente(temp);
+            nuevo.setPrevio(temp.getPrevio());
+            //Conectamos la lista al nuevo nodo
+            temp.getPrevio().setSiguiente(nuevo);
+            temp.setPrevio(nuevo);
+    }
+        tama++;
+    }
+    //Tarea DeleteAt
 }
